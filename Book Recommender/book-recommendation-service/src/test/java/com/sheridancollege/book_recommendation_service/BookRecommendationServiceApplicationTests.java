873110b5bf920f5dfbae3a0a5c0d9737a2aa0037
doh6077 <kimdohee@sheridancollege.ca>
2025-05-21 15:35:48 -0400

    Update .gitignore to untrack unnecessary files and directories

---
+++ Book Recommender/book-recommendation-service/src/test/java/com/sheridancollege/book_recommendation_service/BookRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheridancollege.book_recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
