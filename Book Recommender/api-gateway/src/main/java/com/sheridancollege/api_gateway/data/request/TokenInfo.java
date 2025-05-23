c50066544eea7c0b4293c47806574340e9f23084
doh6077 <kimdohee@sheridancollege.ca>
2025-05-23 16:53:41 -0400

    Add TokenInfo DTO for handling Firebase authentication token requests

---
+++ Book Recommender/api-gateway/src/main/java/com/sheridancollege/api_gateway/data/request/TokenInfo.java
@@ -0,0 +1,11 @@
+package main.java.com.sheridancollege.api_gateway.data.request;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TokenInfo {
+    private String idToken;
+}
